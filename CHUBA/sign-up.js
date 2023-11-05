76a90a01fb6761a7d0c8ad04a39a46549649e22c
Shilekhaffy <salauolanshile@gmail.com>
2023-11-05 11:27:39 +0100

    initial commit

---
+++ CHUBA/sign-up.js
@@ -0,0 +1,17 @@
+//  // To get the checkbox
+//  var checkBox = document.getElementById("naviToggle");
+//  // To get the navigation
+//    var navpage = document.getElementById("nav");
+
+//   function myFunction() {
+ 
+//  // If the checkbox is checked, display the navigation
+
+//  if (checkBox.checked == true){
+   
+//    navpage.style.display = "block";
+//  }
+//   else {
+//    navpage.style.display = "none";
+//  }
+//  }
